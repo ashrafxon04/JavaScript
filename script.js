e62f6a1b2570306bc04c3163f09451de1c58ba04
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-02-24 13:34:33 +0500

    lesson 2

---
+++ script.js
@@ -0,0 +1,95 @@
+// let a = 1 , b = 1;
+
+// console.log(a, b);
+
+// let c = 1 , b = 1;
+
+// console.log( c + ++b); // 1 + 1 = 3  ++b ==> 1qushib beradi
+// console.log( c + b++); // 1 + 1 = 2  b++ ==> b gacha bulgan qiymatni hisoblaydi
+
+
+//==============
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+
+// console.log(x); // --> javob 5 
+
+// ==============
+
+// console.log("" + 1 + 0); // javobi 10
+
+//==============
+
+// console.log("" - 1 + 0); // javob -1
+
+//=============
+
+// console.log(true + false); // javob 1 chiqadi
+
+//===========
+
+// console.log(6 / "3"); 
+
+// ===========
+
+// console.log("2" * "5");  // => javobi 10
+
+// ===============
+
+// console.log(4 + 5 + "px"); // javob 9px
+
+// ===============
+
+// console.log("$" + 4 + 5); // = > $ 45
+
+// ===============
+
+// console.log("4" - 2); // => javob 2
+
+// ===============
+
+// console.log("4px" - 2) // ==> javob NaN
+
+// ===============
+
+// console.log(" -9 " + 5); // == javob -9 5 chunki nirinchi string deb qaralyabdi
+
+// ===============
+
+// console.log(" -9 " - 5); // == javob -14
+
+// ===============
+
+// console.log(null + 1);  // === javob 1 chiqadi
+
+//================
+
+// console.log(undefined + 1); // javon NaN chiqadi
+
+//=================
+
+// console.log("n/ t/" - 2); // javon NaN  chiqadi
+
+
+// Comparision
+
+// console.log(5 > 4); // javob true
+
+//===============
+
+// console.log("apple" >"pinapple"); // javob folse chiqadi
+
+//=================
+
+// console.log("2" >"2"); // javob true chunki type ga qarayabdi
+
+//=================
+
+// console.log(undefined == null); // true 
+// console.log(undefined === null);// folse chunki qatiy taqqoslayabdi
+
+
+// console.log(null == "\n0\n"); // javob folse
+
+// console.log(null + "\n0\n");// javobi null ,0 va new line.
