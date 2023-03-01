cf0b9f52bc00f5994bab5c7f68e3f9be479514ba
ashrafxon04 <ashrafyaxyoyev@gmail.com>
2023-03-01 10:33:10 +0500

    lesson3

---
+++ lesoon3/index.js
@@ -0,0 +1,88 @@
+//lesson 3
+
+// let nameJS = "ECMAScript";
+// if (nameJS == "ECMAScript") {
+//   console.log("Right");
+// } else {
+//   console.log("Your don't know");
+// }
+
+//========================
+// let nameJS = "ECMAScript";
+// nameJS == "ECMAScript" ? console.log("Right") : console.log("Your don't know");
+
+//========================
+// let num = 0;
+// if (num > 0) {
+//   console.log(1);
+// } else if (num < 0) {
+//   console.log(-1);
+// } else {
+//   console.log(0);
+// }
+
+// console.log( new Date().getDay());
+// let week = 8 //new Date().getDay()
+
+
+// switch(week){
+//   case 1: console.log('monday');
+//   break;
+//   case 2: console.log('tuesday');
+//   break;
+//   case 2: console.log('wednesday');
+//   break;
+//   case 2: console.log('thursday');
+//   break;
+//   case 2: console.log('friday');
+//   break;
+//   case 2: console.log('sunday');
+//   break;
+//   default:
+//   console.log('information is not defined');
+// }
+
+//==============================================================================
+
+// let nameJs = "Ecmascript";
+
+// if (nameJs == "Ecmascript") {
+//     console.log("right");
+// } else {
+//      console.log("you don't know ");
+//      }
+
+
+// for (let i = 0; i <= 10; i++ )
+// { console.log(i);}
+
+//===============================================================================
+
+// for (i = 0 ; i <= 10 ; i++){
+//     for (j = 1 ; j <= 10; j++)
+//     console.log(`${i} * ${j} = ${i*j}`);
+// }
+// KARRA JADVAL
+
+
+
+//tub soon
+
+
+// let num =100;
+
+// for(let i = 1;i < num; i++){
+//     tub=true;
+//     for(let j = 2; j < i; j++){
+//         if(i%j==0){
+//             tub=false;
+//             break;
+//         }
+//     }
+//     if(tub==true){
+//         console.log(i);
+//     }
+// }
+
+
+
